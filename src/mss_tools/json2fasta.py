94d4cc11ba0a7109ff223101d12aedc66464ff92
yt <yt@nig.ac.jp>
2025-02-24 15:40:50 +0900

    minor bug fix

--- src/mss_tools/json2fasta.py
+++ src/mss_tools/json2fasta.py
@@ -23,7 +23,7 @@ def json_to_gene_seq(feature_json: Dict, entry_json: Dict) -> Tuple[str, str, st
         translate = ""
     nucleotide = str(nucleotide).upper()
     if gene:
-        description = f"{locus_tag} {product} [gene={gene}] [type={feature_type}] [location={entry_json["name"]}:{location}]"
+        description = f"{locus_tag} {product} [gene={gene}] [type={feature_type}] [location={entry_json['name']}:{location}]"
     else:
         description = f"{locus_tag} {product} [type={feature_type}] [location={entry_json["name"]}:{location}]"
     return description, nucleotide, translate
