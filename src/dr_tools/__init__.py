8e3b02e5394336955bd4646733dd5541cd4b97d3
yt <yt@nig.ac.jp>
2025-02-25 15:21:04 +0900

    minor fix

--- src/dr_tools/__init__.py
+++ src/dr_tools/__init__.py
@@ -1,9 +1,9 @@
 from dr_tools.MSS import MSS
-from dr_tools.ann2json import ann2json_for_dfast as mss_ann2json
-from dr_tools.json2ann import json2ann as mss_json2ann
+from dr_tools.ann2json import ann2json_for_dfast as drt_ann2json
+from dr_tools.json2ann import json2ann as drt_json2ann
 from dr_tools.json2biopython import json_to_seqrecords
-from dr_tools.json2fasta import json2fasta as mss_json2fasta
-from dr_tools.json2stats import json2stats_for_dfast as mss_json2stats
+from dr_tools.json2fasta import json2fasta as drt_json2fasta
+from dr_tools.json2stats import json2stats_for_dfast as drt_json2stats
 
 def hello():
     print("Hello world")
